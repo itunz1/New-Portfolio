964ea815b9721838ba560b5b3d3a9b298a17b445
itunz1 <stx_jose@hotmail.com>
2023-03-04 13:37:47 -0400

    contactSectionResponsive

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -113,68 +113,68 @@ export default function Contact() {
 
 
   return (
-    <div>
+    <div className=''>
       <ToastContainer limit={1} />
-      <h3 className='mb-16 text-5xl font-bold text-center text-white'>Contact Me</h3>
-      <div className='flex max-h-[700px]'>
-        <div className='py-20 px-14 bg-[#2a2c39] lg:w-[35%]'>
-          <ul>
-            <li className='mb-5 py-7 px-9 bg-[#252734]'>
-              <h6 className='mb-5 text-white'>Phone</h6>
-              <p className='text-[#858792]'>(+58) 414 8524974</p>
+      <h3 className='mb-5 text-3xl font-bold text-center text-white md:text-4xl lg:text-5xl'>Contact Me</h3>
+      <div className='flex flex-col-reverse md:flex-row md:max-h-[700px] justify-center'>
+        <div className='md:w-[27%] mx-auto px-3 mt-5 w-[95%] md:px-0 md:ml-4 md:mt-[4.5rem] lg:mt-auto md:h-fit lg:h-fit lg:ml-auto lg:py-8 lg:px-12 lg:w-[35%] bg-[#2a2c39] justify-center flex'>
+          <ul className='grid justify-around w-full grid-cols-2 gap-x-5 md:gap-0 md:w-11/12 lg:w-auto lg:px-auto lg:ml-0 md:flex md:flex-col'>
+            <li className='contact-info'>
+              <h6 className='text-sm text-white sm:mb-2 lg:mb-5 lg:text-base'>Phone</h6>
+              <p className='text-[#858792] text-xs sm:text-sm lg:text-base'>(+58) 414 8524974</p>
             </li>
-            <li className='mb-5 py-7 px-9 bg-[#252734]'>
-              <h6 className='mb-5 text-white'>Email</h6>
-              <p className='text-[#858792]'>stx_jose@hotmail.com</p>
+            <li className='contact-info'>
+              <h6 className='text-sm text-white sm:mb-2 lg:mb-5 lg:text-base'>Email</h6>
+              <p className='text-[#858792] text-xs sm:text-sm lg:text-base'>stx_jose@hotmail.com</p>
             </li>
-            <li className='mb-5 py-7 px-9 bg-[#252734]'>
-              <h6 className='mb-5 text-white'>Location</h6>
-              <p className='text-[#858792]'>Venezuela</p>
+            <li className='contact-info'>
+              <h6 className='text-sm text-white sm:mb-2 lg:mb-5 lg:text-base'>Location</h6>
+              <p className='text-[#858792] text-xs sm:text-sm lg:text-base'>Venezuela</p>
             </li>
-            <li className='mb-5 py-7 px-9 bg-[#252734]'>
-              <h6 className='mb-5 text-white'>Address</h6>
-              <p className='text-[#858792]'>Puerto Ordaz - Altavista</p>
+            <li className='contact-info'>
+              <h6 className='text-sm text-white sm:mb-2 lg:mb-5 lg:text-base'>Address</h6>
+              <p className='text-[#858792] text-xs sm:text-sm lg:text-base'>Puerto Ordaz - Altavista</p>
             </li>
           </ul>
         </div>
 
-        <div className='lg:w-[65%] '>
-          <form className='flex flex-col justify-between w-full p-20 mt-0 text-white' onSubmit={handleSubmit}>
-            <div className='flex gap-10 mb-10'>
-              <div className='w-6/12'>
-                <label className='block mb-2'>Name</label>
-                <input className='w-full h-10 bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-7'
+        <div className='p-7 md:p-0 md:w-[75%] lg:w-[65%] '>
+          <form className='flex flex-col justify-between w-full mt-0 text-white md:p-12 lg:p-20' onSubmit={handleSubmit}>
+            <div className='flex flex-col gap-5 mb-5 md:mb-10 md:gap-10 md:flex-row'>
+              <div className='md:w-6/12'>
+                <label className='block mb-1 md:mb-2'>Name</label>
+                <input className='text-sm lg:text-base w-full h-2 md:w-full md:h-10 bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-7'
                   type='text'
                   name='name'
                   value={state.name}
                   onChange={handleChange}
                   placeholder='Name' />
-                {errors.name && (<p className='text-white'>{errors.name}</p>)}
+                {errors.name && (<p className='text-xs text-[#fff59d] md:text-sm'>{errors.name}</p>)}
               </div>
 
-              <div className='w-6/12'>
-                <label className='block mb-2'>Email</label>
-                <input className='w-full h-10 bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-7'
+              <div className='md:w-6/12'>
+                <label className='block mb-1 md:mb-2'>Email</label>
+                <input className='text-sm lg:text-base w-full h-2 md:w-full md:h-10 bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-7'
                   type='email'
                   name='email'
                   value={state.email}
                   onChange={handleChange}
                   placeholder='Email' />
-                {errors.email && (<p className='text-white'>{errors.email}</p>)}
+                {errors.email && (<p className='text-xs text-[#fff59d] md:text-sm'>{errors.email}</p>)}
               </div>
             </div>
 
             <div className='flex flex-col'>
-              <label className='mb-2'>Subject</label>
-              <input className='h-10 mb-10 bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-7'
+              <label className='mb-1 md:mb-2'>Subject</label>
+              <input className='text-sm lg:text-base h-[4rem] md:h-10 mb-5 md:mb-10 bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-7'
                 type='subject'
                 name='subject'
                 value={state.subject}
                 onChange={handleChange}
                 placeholder='Subject' />
 
-              <label className='mb-2'>Message</label>
-              <textarea className='max-h-[300px] h-[240px] bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-5'
+              <label className='mb-1 md:mb-2'>Message</label>
+              <textarea className='text-sm lg:text-base h-[230px] md:max-h-[300px] md:h-[240px] bg-transparent border-[1px] border-[rgba(255,255,255,.1)] p-5'
                 name='message'
                 onChange={handleChange}
                 placeholder='Message' />
